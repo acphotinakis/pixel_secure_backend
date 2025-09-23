5f768f3803962cf383aacf30219abccad8df4e6e
Andrew Photinakis <acp7795@rit.edu>
2025-09-23 15:37:17 -0400

    updating

--- src/main/java/com/videogamedb/app/exceptions/UnauthorizedAccessException.java
+++
@@ -1,7 +0,0 @@
-package com.videogamedb.app.exceptions;
-
-public class UnauthorizedAccessException extends RuntimeException {
-    public UnauthorizedAccessException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/videogamedb/app/exceptions/ValidationException.java
+++
@@ -1,7 +0,0 @@
-package com.videogamedb.app.exceptions;
-
-public class ValidationException extends RuntimeException {
-    public ValidationException(String message) {
-        super(message);
-    }
-}
